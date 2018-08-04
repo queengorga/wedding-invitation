08c87d4df6e52e5a2343515182b908db9a12d311
Victoria Allen <vickmae.allen@gmail.com>
2018-08-03 22:41:35 -0700

    Edited wedding and details page; Added form validation

---
+++ rsvp.js
@@ -0,0 +1,61 @@
+function formValidation() 
+{
+    
+    var firstname = document.registration.first_name;
+    var lastname = document.registration.last_name;
+    var uemail = document.registration.email;
+    var gattend = document.registration.guest_attend;
+    var gname = document.registration.guest_name;
+    var gmenu = document.registration.guest_menu;
+    
+        
+        if (allLetter(fname)) {
+        if (allLetter(lname)) {
+        if (ValidateEmail(uemail)) {
+        if (attendselect(gattend)) {
+        if (allleter(gname)) {
+        if (menuselect(gmenu)) {
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+    
+    return false;
+}
+
+function allLetter(fname) {
+    var letters = /^[A-Za-z]+$/;
+    if (fname.value.match(letters)) {
+        return true;
+    }
+    else {
+        alert('First must have alphabet characters only');
+        fname.focus();
+        return false;
+    }
+}
+
+function attendselect(ucountry) {
+    if (ucountry.value == "Default") {
+        alert('Select your country from the list');
+        ucountry.focus();
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function ValidateEmail(uemail) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (uemail.value.match(mailformat)) {
+        return true;
+    }
+    else {
+        alert("You have entered an invalid email address!");
+        uemail.focus();
+        return false;
+    }
+}
